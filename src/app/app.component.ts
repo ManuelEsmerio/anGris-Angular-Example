63c0229bc47c7ba82a92ffe51a5f96d7924ce5dd
Manuel Esmerio Garcia <manuel.esmerio1995@gmail.com>
2021-11-02 11:47:15 -0600

    Example agGrid
    In this example we could lear how to use agGrid and how to customize it as genearted sortable, filter, group and how to read the seleted rows with Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,54 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { ColDef } from 'ag-grid-community';
+import { AgGridAngular } from 'ag-grid-angular';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  @ViewChild("agGrid", { static: false }) agGrid!: AgGridAngular;
+
   title = 'ngGrid';
+
+  columnDefs: ColDef[] = [
+    {
+      headerName: "Make",
+      field: "make",
+      rowGroup: true
+    },
+    { headerName: "Price", field: "price", sortable: true, filter: true }
+  ]
+
+  autoGroupColumnDef = {
+    headerName: "Model",
+    field: "model",
+    cellRenderer: "agGroupCellRenderer",
+    cellRendererParams: {
+      checkbox: true
+    }
+  }
+
+  rowData!: Observable<any[]>;
+
+  constructor(private httpClient: HttpClient) { }
+
+  ngOnInit(): void {
+    this.rowData = this.httpClient.get<any[]>('https://www.ag-grid.com/example-assets/row-data.json');
+  }
+
+  getSelectedRows(): void {
+    console.log("Click Selected Rows");
+
+    const selectedData = this.agGrid.api.getSelectedNodes()
+      .map(node => node.data)
+    // .map(node => `${node.make} ${node.model}`)
+    // .join(", ");
+
+    console.log(selectedData);
+  }
 }
